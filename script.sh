69bb78bb0082d4d94d5a63512f04a8551e24fc6e
Unknown <nntoan@apcs.vn>
2018-07-08 11:45:24 +0700

    Update script

--- script.sh
+++ script.sh
@@ -26,34 +26,33 @@ starttime=$(date +%s)
 
 # continue to poll
 # we either get a successful response, or reach TIMEOUT
-while test "$(($(date +%s)-starttime))" -lt "$TIMEOUT" -a $rc -ne 0 || $rc -ne 2 
+while test "$(($(date +%s)-starttime))" -lt "$TIMEOUT" -a $rc -ne 0 -a $rc -ne 2 
 do
     sleep $DELAY
-    echo "Attempting to Start network '$NETWORK' ...$(($(date +%s)-starttime)) secs"
+    echo "Attempting to Start network '$NETWORK' in $(($(date +%s)-starttime)) secs ... "
     set -x
     composer network start -c $PEER_ADMIN_1 -n $NETWORK -V $VERSION -o endorsementPolicyFile=$EPF -A $USER_1 -C $USER_1/admin-pub.pem -A $USER_2 -C $USER_2/admin-pub.pem >&log.txt
     res=$?
     set +x
-    test $res -eq 0 && VALUE=$(cat log.txt | awk '/The connection to the network was successfully tested/ {print $NF}')
-    echo $res
-    echo $VALUE
+    test $res -eq 1 && VALUE=$(cat log.txt | awk '/The connection to the network was successfully tested/ {print $NF}')
     test "$VALUE" = "$EXPECTED_RESULT" && let rc=0
-    test $res -eq 1 && VALUE=$(cat log.txt | awk '/status: 500, message: chaincode exists/ {print $NF}')
-    echo $res
+    test $res -eq 1 && VALUE=$(cat log.txt | awk '/status: 500, message: chaincode exists/{print $NF; exit}')
     echo $VALUE
+    echo "$EXPECTED_RESULT)"
     test "$VALUE" = "$EXPECTED_RESULT)" && let rc=2
+    echo $rc
 done
 echo
 cat log.txt
 if test $rc -eq 0 ; then
     echo "===================== Starting network '$NETWORK' on channel '$CHANNEL_NAME' is successful ===================== "    
 fi
-
+echo $rc
 if test $rc -eq 2 ; then     
-    echo "===================== Starting network '$NETWORK' on channel '$CHANNEL_NAME' is started ===================== "    
+    echo "===================== Network '$NETWORK' on channel '$CHANNEL_NAME' is started ===================== "    
 else
     echo "!!!!!!!!!!!!!!! Starting network '$NETWORK' on channel '$CHANNEL_NAME' is FAIL !!!!!!!!!!!!!!!!"
-    echo "!!!!!!!!!!!!!!! Possible reasons: network is starting !!!!!!!!!!!!!!!!!!!!"
+    echo "!!!!!!!!!!!!!!! Possible reasons: network is started already !!!!!!!!!!!!!!!!!!!!"
     exit 1
 fi
 
@@ -66,4 +65,3 @@ composer card create -p $CONNECTION_2 -u $USER_2 -n $NETWORK -c $USER_2/admin-pu
 composer card delete -c $USER_2@$NETWORK
 composer card import -f $USER_2@$NETWORK.card
 composer network ping -c $USER_2@$NETWORK
-
